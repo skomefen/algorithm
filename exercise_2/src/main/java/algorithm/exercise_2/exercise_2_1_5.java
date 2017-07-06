83dbf4e4a665bf4116e758efd0d7c4a06f8800d7
skomefen <1072760797@qq.com>
2017-07-06 22:25:19 +0800

    添加第二章课堂练习和课后练习

---
+++ exercise_2/src/main/java/algorithm/exercise_2/exercise_2_1_5.java
@@ -0,0 +1,55 @@
+package algorithm.exercise_2;
+
+import util.Heap;
+import util.Insertion;
+import util.Merge;
+import util.Quick;
+import util.Selection;
+import util.Shell;
+import util.StdOut;
+import util.StdRandom;
+import util.Stopwatch;
+
+public class exercise_2_1_5 { 
+	public static double time(String alg, Comparable[] a) {   
+		Stopwatch timer = new Stopwatch();    
+		if (alg.equals("Insertion")) 
+			Insertion.sort(a); 
+		if (alg.equals("Selection")) 
+			Selection.sort(a);
+		if (alg.equals("Shell"))
+			Shell.sort(a);
+		if (alg.equals("Merge"))
+			Merge.sort(a);
+		if (alg.equals("Quick"))
+			Quick.sort(a);
+		if (alg.equals("Heap"))
+			Heap.sort(a);
+		return timer.elapsedTime();
+	}
+
+	public static double timeRandomInput(String alg, int N, int T){ 
+		// 使用算法1将T个长度为N的数组排序
+		double total = 0.0; 
+		Double[] a = new Double[N];  
+		for (int t = 0; t < T; t++){ 
+			// 进行一次测试（生成一个数组并排序)  
+			for (int i = 0; i < N; i++)    
+				a[i] = StdRandom.uniform();     
+			total += time(alg, a);       
+		}       
+		return total;    
+	}
+	public static void main(String[] args) {  
+		String alg1 = "Insertion";    
+		String alg2 = "Selection";   
+		int N = Integer.parseInt("10");
+		int T = Integer.parseInt("8");
+		double t1 = timeRandomInput(alg1, N, T); 
+		// 算法1的总时间
+		double t2 = timeRandomInput(alg2, N, T); 
+		// 算法2的总时间 
+		StdOut.printf("For %d random Doubles\n    %s is", N, alg1);
+		StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);  
+	}
+}
